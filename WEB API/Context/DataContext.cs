e02ecc06e874915c1be997738faa0fbbb8dbf83a
Sreejith R <sreejith.rajendran@outlook.com>
2021-05-31 20:37:51 +0530

    added entity framework packages and done the first migration

---
+++ WEB API/Context/DataContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WEB_API.Models;
+
+namespace WEB_API.Context
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options) { }
+
+        public DbSet<City> Cities { get; set; }
+    }
+}
